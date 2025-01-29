efcawesome/tModLoader

commit a34e2c1385cedc228e9eba27b68869309ea42dc7

Minor Decraft Fixes (#3993)
* Bugfix

* DecraftAvailable checks notDecraftable

* Shimmer decraft (not transforms) now cap at max stack not 9999

* Obsoleted ConsumeItem and added ConsumeIngredient

+ fixed stacking bug for other type.

* Change requested by ChickenBones

patches/tModLoader/Terraria/Recipe.TML.cs
@@ -19,32 +19,40 @@ namespace Terraria;
 /// </summary>
 public partial class Recipe
 {
+	[Obsolete($"Replaced by {nameof(IngredientQuantityRules)} due to not accounting for shimmer decrafting")]
 	public static class ConsumptionRules
 	{
-		/// <summary> Gives 1/3 chance for every ingredient to not be consumed, if used at an alchemy table. (!) This behavior is already automatically given to all items that can be made at a placed bottle tile. </summary>
-		public static ConsumeItemCallback Alchemy = (Recipe recipe, int type, ref int amount) => {
-			if (!Main.LocalPlayer.alchemyTable) return;
+		[Obsolete($"Replaced by {nameof(IngredientQuantityRules)}.{nameof(IngredientQuantityRules.Alchemy)} due to not accounting for shimmer decrafting")]
+		public static ConsumeItemCallback Alchemy => (Recipe recipe, int type, ref int amount) => IngredientQuantityRules.Alchemy(recipe, type, ref amount, false);
+	}
 
-			int amountUsed = 0;
-
-			for (int i = 0; i < amount; i++) {
-				if (!Main.rand.NextBool(3))
-					amountUsed++;
+	public static class IngredientQuantityRules
+	{
+		/// <summary> Gives 1/3 chance for every ingredient to not be consumed, if used at an alchemy table. (!) This behaviour is already automatically given to all items that can be made at a placed bottle tile. </summary>
+		public static IngredientQuantityCallback Alchemy = (Recipe recipe, int type, ref int amount, bool isDecrafting) => {
+			if (!Main.LocalPlayer.alchemyTable && !isDecrafting)
+				return;
+			for (int i = amount; i > 0; i--) {
+				if (Main.rand.NextBool(3))
+					amount--;
 			}
-
-			amount = amountUsed;
 		};
 	}
 
 	public readonly Mod Mod;
-	public readonly List<Condition> Conditions = new List<Condition>();
-	public readonly List<Condition> DecraftConditions = new List<Condition>();
+	public readonly List<Condition> Conditions = new();
+	public readonly List<Condition> DecraftConditions = new();
 
 	public delegate void OnCraftCallback(Recipe recipe, Item item, List<Item> consumedItems, Item destinationStack);
-	public delegate void ConsumeItemCallback(Recipe recipe, int type, ref int amount);
 
+	[Obsolete($"Replaced by {nameof(IngredientQuantityCallback)} due to not accounting for shimmer decrafting")]
+	public delegate void ConsumeItemCallback(Recipe recipe, int type, ref int amount);
+	/// <summary>
+	/// Called for both <see cref="Create()"/> and <see cref="Item.GetShimmered"/>, using <paramref name="isDecrafting"/> = <see langword="true"/> to denote a shimmer operation
+	/// </summary>
+	public delegate void IngredientQuantityCallback(Recipe recipe, int type, ref int amount, bool isDecrafting);
 	internal OnCraftCallback OnCraftHooks { get; private set; }
-	internal ConsumeItemCallback ConsumeItemHooks { get; private set; }
+	internal IngredientQuantityCallback ConsumeIngredientHooks { get; private set; }
 
 	private void AddGroup(int id)
 	{
@@ -62,6 +70,11 @@ public partial class Recipe
 	/// Any recipe with this flag won't be shown in game.
 	/// </summary>
 	public bool Disabled { get; private set; }
+
+	/// <summary>
+	/// Any recipe with this flag won't be decrafted in shimmer.
+	/// </summary>
+	public bool DecraftDisabled => notDecraftable;
 
 	/// <summary>
 	/// Adds an ingredient to this recipe with the given item type and stack size. Ex: <c>recipe.AddIngredient(ItemID.IronAxe)</c>
@@ -304,9 +317,21 @@ public partial class Recipe
 	/// <summary>
 	/// Sets a callback that allows you to determine how many of a certain ingredient is consumed when this recipe is used. Return the number of ingredients that will actually be consumed. By default returns numRequired.
 	/// </summary>
+
+	[Obsolete($"Replaced by {nameof(AddConsumeIngredientCallback)} due to not accounting for shimmer decrafting")]
 	public Recipe AddConsumeItemCallback(ConsumeItemCallback callback)
 	{
-		ConsumeItemHooks += callback;
+		ConsumeIngredientHooks += (Recipe recipe, int type, ref int num, bool decraft) => callback(recipe, type, ref num);
+
+		return this;
+	}
+
+	/// <summary>
+	/// Sets a callback that allows you to determine how many of a certain ingredient is consumed when this recipe is used. Return the number of ingredients that will actually be consumed. By default returns numRequired.
+	/// </summary>
+	public Recipe AddConsumeIngredientCallback(IngredientQuantityCallback callback)
+	{
+		ConsumeIngredientHooks += callback;
 
 		return this;
 	}
@@ -406,13 +431,12 @@ public partial class Recipe
 		clone.anyPressurePlate = anyPressurePlate;
 		clone.anySand = anySand;
 		clone.anyFragment = anyFragment;
-		clone.alchemy = alchemy;
 		clone.needSnowBiome = needSnowBiome;
 		clone.needGraveyardBiome = needGraveyardBiome;
 		clone.needEverythingSeed = needEverythingSeed;
 
 		clone.OnCraftHooks = OnCraftHooks;
-		clone.ConsumeItemHooks = ConsumeItemHooks;
+		clone.ConsumeIngredientHooks = ConsumeIngredientHooks;
 		foreach (Condition condition in Conditions) {
 			clone.AddCondition(condition);
 		}
@@ -424,7 +448,7 @@ public partial class Recipe
 		// A subsequent call to Register() will re-add this hook if Bottles is a required tile, so we remove
 		// it here to not have multiple duplicate hooks.
 		if (clone.requiredTile.Contains(TileID.Bottles))
-			clone.ConsumeItemHooks -= ConsumptionRules.Alchemy;
+			clone.ConsumeIngredientHooks -= IngredientQuantityRules.Alchemy;
 
 		return clone;
 	}
@@ -442,7 +466,7 @@ public partial class Recipe
 			throw new RecipeException("There was an attempt to register an already registered recipe.");
 
 		if (requiredTile.Contains(TileID.Bottles))
-			AddConsumeItemCallback(ConsumptionRules.Alchemy);
+			AddConsumeIngredientCallback(IngredientQuantityRules.Alchemy);
 
 		if (numRecipes >= maxRecipes) {
 			maxRecipes += 500;

ExampleMod/Common/ExampleRecipeCallbacks.cs
@@ -7,8 +7,8 @@ namespace ExampleMod.Common
 	public static class ExampleRecipeCallbacks
 	{
 		// ConsumeItemCallbacks - These are used to adjust the number of ingredients consumed by recipes, similar to Alchemy Table - See https://github.com/tModLoader/tModLoader/wiki/Intermediate-Recipes#custom-item-consumption
-		public static void DontConsumeChain(Recipe recipe, int type, ref int amount) {
-			if (type == ItemID.Chain) {
+		public static void DontConsumeChain(Recipe recipe, int type, ref int amount, bool isDecrafting) {
+			if (type == ItemID.Chain) { // since there is no check for isDecrafting this will not return chains on shimmer either
 				amount = 0;
 			}
 		}

ExampleMod/Content/ExampleRecipes.cs
@@ -142,11 +142,11 @@ namespace ExampleMod.Content
 			clonedRecipe.Register();
 
 			// Recipes can also contain custom item consumption logic, similar to how the Alchemy Table causes potion recipes to consume less ingredients: See https://github.com/tModLoader/tModLoader/wiki/Intermediate-Recipes#custom-item-consumption for more information.
-			// This example requires the Chain item as an ingredient, but the DontConsumeChain ConsumeItemCallback causes the Chain to not be consumed
+			// This example requires the Chain item as an ingredient, but the DontConsumeChain ConsumeIngredientCallback causes the Chain to not be consumed
 			Recipe.Create(ItemID.AlphabetStatueJ)
 				.AddIngredient(ItemID.StoneBlock, 10)
 				.AddIngredient(ItemID.Chain)
-				.AddConsumeItemCallback(ExampleRecipeCallbacks.DontConsumeChain)
+				.AddConsumeIngredientCallback(ExampleRecipeCallbacks.DontConsumeChain)
 				.AddTile(TileID.HeavyWorkBench)
 				.Register();
 

patches/tModLoader/Terraria/Item.cs.patch
@@ -2019,6 +2019,43 @@
  
  			Main.player[Main.myPlayer].AddCoinLuck(base.Center, stack);
  			NetMessage.SendData(146, -1, -1, null, 1, (int)base.Center.X, (int)base.Center.Y, stack);
+@@ -47808,10 +_,34 @@
+ 			shimmered = true;
+ 		}
+ 		else if (ItemID.Sets.ShimmerTransformToItem[shimmerEquivalentType] > 0) {
++			// Since 1.4.5 seems to add item stacking (April 2024 state of the game) this is likely a temporary fix. All variables are renamed as such so I can read this later. (Copy paste + modify from decrafting code) - Kogsey
+-			int num8 = stack;
++			int stackSize = stack;
+ 			SetDefaults(ItemID.Sets.ShimmerTransformToItem[shimmerEquivalentType]);
+-			stack = num8;
++			stack = stackSize;
++			if (stack > maxStack)
++				stack = maxStack;
++			stackSize -= stack;
+ 			shimmered = true;
++			int spawnCount = 0;
++
++			while (stackSize > 0) {
++				spawnCount++;
++
++				int newItemIndex = NewItem(GetItemSource_Misc(8), (int)position.X, (int)position.Y, width, height, type);
++				Item item = Main.item[newItemIndex];
++				item.stack = stackSize;
++				if (item.stack > maxStack)
++					item.stack = maxStack;
++				stackSize -= item.stack;
++				item.shimmerTime = 1f;
++				item.shimmered = true;
++				item.shimmerWet = true;
++				item.wet = true;
++				item.velocity *= 0.1f;
++				item.playerIndexTheItemIsReservedFor = Main.myPlayer;
++
++				NetMessage.SendData(145, -1, -1, null, newItemIndex, 1f);
++			}
+ 		}
+ 		else if (type == 4986) {
+ 			if (NPC.unlockedSlimeRainbowSpawn)
 @@ -47857,7 +_,7 @@
  			int num13 = FindDecraftAmount();
  			Recipe recipe = Main.recipe[decraftingRecipeIndex];
@@ -2028,6 +2065,31 @@
  			IEnumerable<Item> enumerable = recipe.requiredItem;
  			if (recipe.customShimmerResults != null)
  				enumerable = recipe.customShimmerResults;
+@@ -47869,17 +_,22 @@
+ 
+ 				num15++;
+ 				int num16 = num13 * item2.stack;
++
++				/* TML: Recipe.alchemy is removed due to being replaced by consume item hooks
+ 				if (recipe.alchemy) {
+ 					for (int num17 = num16; num17 > 0; num17--) {
+ 						if (Main.rand.Next(3) == 0)
+ 							num16--;
+ 					}
+ 				}
++				*/
++
++				RecipeLoader.ConsumeIngredient(recipe, item2.type, ref num16, true);
+ 
+ 				while (num16 > 0) {
+ 					int num18 = num16;
+-					if (num18 > 9999)
+-						num18 = 9999;
++					if (num18 > item2.maxStack)
++						num18 = item2.maxStack;
+ 
+ 					num16 -= num18;
+ 					int num19 = NewItem(GetItemSource_Misc(8), (int)position.X, (int)position.Y, width, height, item2.type);
 @@ -48004,7 +_,7 @@
  		if (Main.netMode == 1) {
  			int num = (int)(position.X + (float)(width / 2)) / 16;

patches/tModLoader/Terraria/ModLoader/RecipeLoader.cs
@@ -167,7 +167,7 @@ public static class RecipeLoader
 	/// <returns>Whether or not the conditions are met for this recipe.</returns>
 	public static bool DecraftAvailable(Recipe recipe)
 	{
-		return recipe.DecraftConditions.All(c => c.IsMet());
+		return !recipe.notDecraftable && recipe.DecraftConditions.All(c => c.IsMet());
 	}
 
 	/// <summary>
@@ -196,13 +196,26 @@ public static class RecipeLoader
 	}
 
 	/// <summary>
-	/// Allows to edit the amount of item the player uses in a recipe.
+	/// Allows to edit the amount of item the player uses in a recipe. Also used to decide the amount a shimmer transformation returns
 	/// </summary>
 	/// <param name="recipe">The recipe used for the craft.</param>
 	/// <param name="type">Type of the ingredient.</param>
 	/// <param name="amount">Modifiable amount of the item consumed.</param>
+	[Obsolete($"Replaced by {nameof(ConsumeIngredient)} due to not accounting for shimmer decrafting")]
 	public static void ConsumeItem(Recipe recipe, int type, ref int amount)
 	{
-		recipe.ConsumeItemHooks?.Invoke(recipe, type, ref amount);
+		ConsumeIngredient(recipe, type, ref amount, false);
+	}
+
+	/// <summary>
+	/// Allows to edit the amount of item the player uses in a recipe. Also used to decide the amount a shimmer transformation returns
+	/// </summary>
+	/// <param name="recipe">The recipe used for the craft.</param>
+	/// <param name="type">Type of the ingredient.</param>
+	/// <param name="amount">Modifiable amount of the item consumed.</param>
+	/// <param name="isDecrafting">If the operation takes place during shimmer decrafting.</param>
+	public static void ConsumeIngredient(Recipe recipe, int type, ref int amount, bool isDecrafting)
+	{
+		recipe.ConsumeIngredientHooks?.Invoke(recipe, type, ref amount, isDecrafting);
 	}
 }

patches/tModLoader/Terraria/Recipe.cs.patch
@@ -60,7 +60,7 @@
 -	public bool anyFragment;
 +	internal bool anyFragment;
 -	public bool alchemy;
-+	internal bool alchemy;
++	//internal bool alchemy; TML: Recipe.alchemy is unused due to being replaced by consume item hooks
 -	public bool needSnowBiome;
 +	internal bool needSnowBiome;
 -	public bool needGraveyardBiome;
@@ -70,7 +70,7 @@
 -	public bool notDecraftable;
 +
 -	public bool crimson;
-+	// Following fields made internal, use DisableDecraft()
++	// Following fields made internal, use DisableDecraft() and DecraftDisabled
 -	public bool corruption;
 +	internal bool notDecraftable;
 +
@@ -166,7 +166,7 @@
  	public Recipe()
  	{
  		for (int i = 0; i < maxRequirements; i++) {
-@@ -131,17 +_,23 @@
+@@ -131,18 +_,25 @@
  			acceptedGroups[i] = -1;
  		}
  	}
@@ -194,14 +194,18 @@
 -
 +		foreach (Item item2 in requiredItem) {
  			int num = item2.stack;
++			/* TML: Recipe.alchemy is unused due to being replaced by consume item hooks
  			if (alchemy && Main.player[Main.myPlayer].alchemyTable) {
  				if (num > 1) {
-@@ -158,25 +_,36 @@
+ 					int num2 = 0;
+@@ -157,26 +_,38 @@
+ 					num = 0;
  				}
  			}
- 
-+			RecipeLoader.ConsumeItem(this, item2.type, ref num);
++			*/
 +
++			RecipeLoader.ConsumeIngredient(this, item2.type, ref num, false);
+ 
  			if (num <= 0)
  				continue;
  
@@ -744,7 +748,7 @@
 +
 +		//TML: The whole method is rewritten.
 +		if (currentRecipe.requiredTile.Count > 0 && currentRecipe.requiredTile[0] == TileID.Bottles)
-+			currentRecipe.AddConsumeItemCallback(ConsumptionRules.Alchemy);
++			currentRecipe.AddConsumeIngredientCallback(IngredientQuantityRules.Alchemy);
 +
 +		static void ReplaceCondition(ref bool field, Condition cond)
 +		{

commit 989fec9b6c6e83081ce967703239ecc3d60112e7

Fix binary compatibility regression from #3993

patches/tModLoader/Terraria/Recipe.TML.cs
@@ -23,7 +23,7 @@ public partial class Recipe
 	public static class ConsumptionRules
 	{
 		[Obsolete($"Replaced by {nameof(IngredientQuantityRules)}.{nameof(IngredientQuantityRules.Alchemy)} due to not accounting for shimmer decrafting")]
-		public static ConsumeItemCallback Alchemy => (Recipe recipe, int type, ref int amount) => IngredientQuantityRules.Alchemy(recipe, type, ref amount, false);
+		public static ConsumeItemCallback Alchemy = (Recipe recipe, int type, ref int amount) => IngredientQuantityRules.Alchemy(recipe, type, ref amount, false);
 	}
 
 	public static class IngredientQuantityRules